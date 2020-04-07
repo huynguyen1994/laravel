8c6169ed3465d76c232c67706a1ecb9236d8b5b5
unknown <huy.nguyen1911994@gmail.com>
2020-04-07 10:59:38 +0700

    change url asset in vendor

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,8 +6,9 @@
     <title>Login</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="{{ asset('images/icons/favicon.ico') }}"/>
-    <link rel="stylesheet" type="text/css" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <?php echo URL::asset('image'); ?>
+    <link rel="icon" type="image/png" href="{{ URL::asset('images/icons/favicon.ico') }}"/>
+    <link rel="stylesheet" type="text/css" href="{{ URL::asset('bootstrap/css/bootstrap.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('fonts/Linearicons-Free-v1.0.0/icon-font.min.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('animate/animate.css') }}">
