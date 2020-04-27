dad867823f96a758df06eb016564c95c0fb201e9
unknown <huy.nguyen1911994@gmail.com>
2020-04-27 13:37:11 +0700

    css and services.php

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -90,7 +90,7 @@ class LoginController extends Controller
     public function logout(Request $request)
     {
         Auth::logout();
-        return view($this->getView('login'));
+        return redirect('/');
     }
 
     /**
